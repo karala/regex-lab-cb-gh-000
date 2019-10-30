0fe7c930ae62a757eb843bf223ecfdbd30316000
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-10-30 11:49:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\^[aeiouAEIOU]/)
-  return true
+  if word.scan(/\^[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
